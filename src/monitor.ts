ee0f8fad3f5dcabdee8ebe0623bdbd816dff6e4a
Nikita Melnikov <nikmel2803@gmail.com>
2023-11-26 23:54:50 +0100

    fmt

--- src/monitor.ts
+++ src/monitor.ts
@@ -3,30 +3,30 @@ import { goerli } from 'npm:viem/chains'
 import { address } from "./constants.ts";
 
 const client = createPublicClient({
-  chain: goerli,
-  transport: http(),
+    chain: goerli,
+    transport: http(),
 })
 
 const main = async () => {
     let latestBlockNumber = await client.getBlockNumber();
     console.log(`Current block number: ${latestBlockNumber}`);
-  
+
     setInterval(async () => {
-      const currentBlockNumber = await client.getBlockNumber();
-      while (currentBlockNumber > latestBlockNumber) {
-        const block = await client.getBlock({ blockNumber: currentBlockNumber, includeTransactions: true })
-        if (block && block.transactions) {
-          console.log(`New block number: ${block.number}`);
-          block.transactions.forEach(tx => {
-            if (tx.to === address) {
-                console.log(`MATCH!!!!`);
-                console.log(`Transaction: from ${tx.from} to ${tx.to} amount: ${tx.value} ETH`);
+        const currentBlockNumber = await client.getBlockNumber();
+        while (currentBlockNumber > latestBlockNumber) {
+            const block = await client.getBlock({ blockNumber: currentBlockNumber, includeTransactions: true })
+            if (block && block.transactions) {
+                console.log(`New block number: ${block.number}`);
+                block.transactions.forEach(tx => {
+                    if (tx.to === address) {
+                        console.log(`MATCH!!!!`);
+                        console.log(`Transaction: from ${tx.from} to ${tx.to} amount: ${tx.value} ETH`);
+                    }
+                });
             }
-          });
+            latestBlockNumber++;
         }
-        latestBlockNumber++;
-      }
     }, 5000); // Poll every 10 seconds
-  };
-  
-  main().catch(e => console.error(e));
+};
+
+main().catch(e => console.error(e));
